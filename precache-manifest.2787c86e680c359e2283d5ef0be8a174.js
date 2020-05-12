5b031152a0f5f471faf8a714ed7a32cf52e748ca
tgoslee <60710670+tgoslee@users.noreply.github.com>
2020-05-12 15:41:13 -0400

    Updates

---
+++ precache-manifest.2787c86e680c359e2283d5ef0be8a174.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b90bb19d3c0cb4fd552400487670bea",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "46cfb34b2255f20a8cac",
+    "url": "/monster-app/static/css/main.6328d8d3.chunk.css"
+  },
+  {
+    "revision": "dda897da56ce2b07ed75",
+    "url": "/monster-app/static/js/2.09ed432d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-app/static/js/2.09ed432d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46cfb34b2255f20a8cac",
+    "url": "/monster-app/static/js/main.4bd36e5c.chunk.js"
+  },
+  {
+    "revision": "6fa8a518723e6e4f5735",
+    "url": "/monster-app/static/js/runtime-main.eb1fd113.js"
+  }
+]);
